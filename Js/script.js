859fa7265515daf75a4ff59c71b0d96c1aa208dc
Ivan <retouch226@gmail.com>
2022-01-19 21:41:17 +0200

    feat: add function closeMenu

--- Js/script.js
+++ Js/script.js
@@ -4,4 +4,14 @@ headerBurger.addEventListener("click", () => {
   const headerMenu = document.querySelector(".header__menu");
   //headerMenu.classList.toggle("menu-mobile");
   headerMenu.classList.toggle("active");
+  headerBurger.classList.toggle("active");
 });
+const menuList = document.querySelector(".menu-list");
+const navLinks = document.querySelectorAll(".nav");
+
+menuList.addEventListener("click", closeMenu);
+function closeMenu(event) {
+  if (event.target.classList.contains("nav")) {
+    event.target.parentNode.parentNode.parentElement.classList.remove("active");
+  }
+}
